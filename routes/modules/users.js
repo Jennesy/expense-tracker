8411b84845c2836a94ffd3357dc8ca483494a957
Jennesy <jennesy.dai@gmail.com>
2021-06-05 23:52:49 +0800

    feat: add user registration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -14,5 +14,18 @@ router.post('/login', (req, res) => {
 router.get('/register', (req, res) => {
   res.render('register')
 })
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('User already exists.')
+        res.render('register', { name, email, password, confirmPassword })
+      }
+      User.create({ name, email, password, confirmPassword })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    })
+})
 // 匯出路由模組
 module.exports = router
