610d98daddd09fe59b32bdffc78cc9e68cb0bf07
Jennesy <jennesy.dai@gmail.com>
2021-06-06 22:46:34 +0800

    feat: add date filter function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,20 +9,43 @@ const getTotalAmount = require('../../models/getTotalAmount')
 
 // 定義首頁路由
 router.get('/', (req, res) => {
+  const errors = []
+  if (req.query.allDate) {
+    req.query.startDate = ''
+    req.query.endDate = ''
+  }
+  if (new Date(req.query.startDate) > new Date(req.query.endDate)) {
+    req.query.startDate = ''
+    req.query.endDate = ''
+    errors.push({ message: '起始日期須早於終止日期。' })
+  }
   const userId = req.user._id
   const filter = req.query.filter || ''
+  const startDate = req.query.startDate || ''
+  const endDate = req.query.endDate || ''
   Record.find({ userId })
     .lean()
     .sort({ date: 'desc' })
     .then(records => {
       const recordList = []
-      if (filter === '') {
-        recordList.push(...records)
+      if (startDate && endDate) {
+        if (filter === '') {
+          recordList.push(...records.filter(record => { return record.date >= new Date(startDate) && record.date <= new Date(endDate) }))
+        } else {
+          recordList.push(...records.filter(record => { return record.date >= new Date(startDate) && record.date <= new Date(endDate) && record.category === filter }))
+        }
       } else {
-        recordList.push(...records.filter(record => { return record.category === filter }))
+        if (filter === '') {
+          recordList.push(...records)
+        } else {
+          recordList.push(...records.filter(record => { return record.category === filter }))
+        }
+        if ((startDate && !endDate) || (!startDate && endDate)) {
+          errors.push({ message: '起訖時間皆須填寫。' })
+        }
       }
       const totalAmount = getTotalAmount(recordList)
-      res.render('index', { recordList, totalAmount, filter })
+      res.render('index', { recordList, totalAmount, filter, startDate, endDate, errors })
     })
 })
 // 匯出路由模組
