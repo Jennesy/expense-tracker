ce00369f7991a358f56d7cbd2a6e8e379a84de48
Jennesy <jennesy.dai@gmail.com>
2021-06-06 20:15:04 +0800

    feat: add facebook login

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,11 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
 const bcrypt = require('bcryptjs')
 const User = require('../models/user')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 module.exports = (app) => {
   app.use(passport.initialize())
@@ -26,7 +30,31 @@ module.exports = (app) => {
       })
       .catch(err => done(err, false))
   }))
-
+  passport.use(new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+      callbackURL: process.env.FACEBOOK_CALLBACK,
+      profileFields: ['email', 'displayName']
+    },
+    (accessToken, refreshToken, profile, done) => {
+      const { name, email } = profile._json
+      User.findOne({ email })
+        .then(user => {
+          if (user) {
+            return done(null, user)
+          }
+          const randomPassword = Math.random().toString(36).slice(-8)
+          return bcrypt
+            .genSalt(10)
+            .then(salt => { return bcrypt.hash(randomPassword, salt) })
+            .then(hash => {
+              return User.create({ name, email, password: hash })
+            })
+            .then(user => { return done(null, user) })
+        })
+        .catch(err => done(err, false))
+    }))
   passport.serializeUser(function (user, done) {
     done(null, user.id)
   })
